5a1d4b26c181d6eafc6fe7af8bb2c543df61db12
AndrewProkopenko <i.andrewprokopenko@gmail.com>
2020-08-11 21:47:50 +0300

    Updates

---
+++ precache-manifest.1d2fd04055d614d51c9c57018b7957cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46515efacda6925a5d4752eca82db7e",
+    "url": "/react_quiz/index.html"
+  },
+  {
+    "revision": "1399046391ed99381264",
+    "url": "/react_quiz/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5bd059fb04cb2602d900",
+    "url": "/react_quiz/static/css/main.afd43e5f.chunk.css"
+  },
+  {
+    "revision": "1399046391ed99381264",
+    "url": "/react_quiz/static/js/2.64bf2afc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react_quiz/static/js/2.64bf2afc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd059fb04cb2602d900",
+    "url": "/react_quiz/static/js/main.5c8cf7fb.chunk.js"
+  },
+  {
+    "revision": "e18128925566336e5765",
+    "url": "/react_quiz/static/js/runtime-main.ac47e39f.js"
+  }
+]);
