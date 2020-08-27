8355f28f75b9e1e94997f8d08104c336aa153d73
AndrewProkopenko <i.andrewprokopenko@gmail.com>
2020-08-27 18:00:08 +0300

    consumer container created

--- src/QuizContainer.js
+++ src/QuizContainer.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import QuizCard from './QuizCard'
-import QuizResult from './QuizResult'
+
 import QuizContext from './context/QuizContext'
+import ConsumerContainer from './ConsumerContainer'
 // import Notification from './Notification/Notification'
 
 import {Container, Row, Col} from 'reactstrap'
@@ -18,29 +18,12 @@ const QuizContainer = () => {
                    {
                         // visible && <Notification color={'dark'} visible={visible} /> 
                    }
-                   <QuizContext.Consumer>
-                       {
-                           context => (
-                                <>
-                                   {
-                                       !context.isShowResult &&
-                                       <QuizCard 
-                                            quections={context.quiz} 
-                                            showResult = { context.showResult}
-                                        /> 
-                                   }
-                                    <div>
-                                        {
-                                            context.isShowResult && 
-                                            <QuizResult 
-                                                result={context.answeredQuections}
-                                            />  
-                                        }
-                                    </div>
-                                </>
-                           ) 
-                       } 
-                       
+                   <QuizContext.Consumer>  
+                        {
+                            context => (
+                                <ConsumerContainer context = {context} />  
+                            )
+                        }
                    </QuizContext.Consumer>
                    
                </Col>
